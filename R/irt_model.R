48e21bed9cb78718db56f384b58dbd1181ee5c86
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2019-02-28 14:57:22 +0100

    Update column output for mirror plots table

--- R/irt_model.R
+++ R/irt_model.R
@@ -470,7 +470,7 @@ simulation_task <- function(model) {
     df <- utils::read.csv(model$dataset, nrows=0)
     columns <- paste(colnames(df), collapse=' ')
     cg <- add_line(cg, paste0("$TABLE ", columns, " FILE=simulation_tab1 FORMAT=,1PE11.4 NOAPPEND ONEHEADER NOPRINT"))
-    cg <- add_line(cg, "$TABLE ID ITEM DV PSI TIME PPRED PWRES FILE=mirror_plot_tab1 NOAPPEND ONEHEADER NOPRINT")
+    cg <- add_line(cg, "$TABLE ID ITEM DV PSI MDV FILE=mirror_plot_tab1 NOAPPEND ONEHEADER NOPRINT")
     cg
 }
 

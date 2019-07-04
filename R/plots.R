ac92d6cb556fa68ca0e65e86ec4383924813f2e3
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-07-04 15:52:51 +0200

    switched to using nrows and page rather than printing to list

--- R/plots.R
+++ R/plots.R
@@ -31,7 +31,7 @@ graded_response_model <- function(data) {
 #' @param items_per_page Default to 8
 #' @return A list of pages
 #' @export
-icc_plots <- function(df, model, items_per_page=8) {
+icc_plots <- function(df, model, items_per_page=NULL, page = 1) {
     scale <- model$scale
     df <- df %>%
         filter_observations() %>%
@@ -57,32 +57,17 @@ icc_plots <- function(df, model, items_per_page=8) {
 
     item_labels <- item_name_list(scale)
 
-    k <- 1
-    plot_list <- list()
-    for (i in seq(1, length(unique_items), by=items_per_page)) {
-        if (length(unique_items) - i + 1 < items_per_page) {
-            # This is the final iteration
-            current_items <- unique_items[i:length(unique_items)]
-        } else {
-            # Next chunk of items
-            current_items <- unique_items[i:(i + items_per_page - 1)]
-        }
-        # FIXME: We could do away with this by using nrows and page options to facet_grid_paginate
-        partial_df <- dplyr::filter(full_df, .data$ITEM %in% current_items)
-        partial_psi_grid <- dplyr::filter(psi_grid, .data$ITEM %in% current_items)
-
-        plot <- ggplot(partial_df, aes(.data$PSI, as.numeric(.data$DV >= .data$CAT))) +
-            geom_point() +
-            geom_smooth(method="gam", method.args=list(family="binomial"), formula=y~s(x, bs="cs")) +
-            geom_line(data=partial_psi_grid, aes(.data$PSI, .data$P), size=1, colour="darkred") +
-            ggforce::facet_grid_paginate(ITEM~.data$CAT, labeller=
-                labeller(ITEM=as_labeller(item_labels), ITEM=label_wrap_gen(20), CAT=as_labeller(score_labels))) +
-            theme_bw(base_size=14, base_family="") +
-            labs(x="PSI", y="Y>=score")
-        plot_list[[k]] <- plot
-        k <- k + 1
-    }
-    plot_list
+    plot <- ggplot(full_df, aes(.data$PSI, as.numeric(.data$DV >= .data$CAT))) +
+        geom_point() +
+        geom_smooth(method="gam", method.args=list(family="binomial"), formula=y~s(x, bs="cs")) +
+        geom_line(data=psi_grid, aes(.data$PSI, .data$P), size=1, colour="darkred") +
+        ggforce::facet_grid_paginate(ITEM~.data$CAT, 
+                                     labeller=labeller(ITEM=as_labeller(item_labels), ITEM=label_wrap_gen(20), CAT=as_labeller(score_labels)),
+                                     nrow = items_per_page, page = page) +
+        theme_bw(base_size=14, base_family="") +
+        labs(x="PSI", y="Y>=score")
+    
+   plot
 }
 
 #' Mirror plots for comparison of original data and simulated data

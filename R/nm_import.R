79aace176fa1f915b3cdeda4bb93a6257d810b3a
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-07-04 11:25:36 +0200

    Added function to read item prms from NM

---
+++ R/nm_import.R
@@ -0,0 +1,24 @@
+#' Read item paramter values from NM table
+#'
+#' @param model An irt_model object
+#' @param lst_file path to the table file
+#'
+#' @return A data frame with item parameters 
+#' @export
+read_item_parameters_from_nmtab <- function(model, nmtab_file){
+    if (!requireNamespace("xpose", quietly = TRUE)) {
+        stop("Package \"xpose\" needed for this function to work. Please install it.",
+             call. = FALSE)
+    }
+    output_table <- xpose::read_nm_tables(nmtab_file, quiet = T)
+    if(!"ITEM" %in% colnames(output_table)) stop("The column ITEM needs to be available in the table.")
+    
+    item_prms <- dplyr::filter(output_table, !duplicated(.data$ITEM)) %>% 
+        dplyr::select(ITEM, dplyr::matches("DIS|DIF|GUE")) %>% 
+        tidyr::gather("parameter", "value", -ITEM)
+    
+    required_prms <- purrr::map_dfr(all_items(model), ~tibble::tibble(ITEM = .x, parameter = item_parameter_names(model, .x)))
+    
+    dplyr::left_join(required_prms, item_prms, by = c("ITEM", "parameter")) %>% 
+        dplyr::rename(item = ITEM)
+}

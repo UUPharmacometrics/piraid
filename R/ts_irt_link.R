87aa2f18bce61ad461bd91f9cce77ccbb228bb53
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2020-04-08 10:56:44 +0200

    add IRT-based link calculation for BI model

--- R/ts_irt_link.R
+++ R/ts_irt_link.R
@@ -1,3 +1,32 @@
+
+# the function calculates the probability mass function for the total score given psi
+pmf_ts <- function(model, psi){
+    if(inherits(model, "SingleGroupClass")){
+        mirt_model <- model
+    }else{
+        mirt_model <- as_mirt_model(model)
+    }
+    pgf <- purrr::map(mirt::extract.mirt(mirt_model, "itemnames"), ~mirt::extract.item(mirt_model, .x)) %>%
+        purrr::map(~mirt::probtrace(.x, as.matrix(psi))) %>% 
+        purrr::map(~as.list(as.data.frame(t(.x)))) %>% 
+        purrr::transpose() %>% 
+        purrr::map(~purrr::reduce(.x, function(x, y) pracma::polymul(y, x), .init = 1)) 
+    
+    do.call(rbind, args = pgf)
+}
+
+mean_score <- function(pmf){
+    if(NCOL(pmf)==1) pmf <- matrix(pmf, nrow = 1)
+    unname(t(matrix(seq(0, ncol(pmf)-1), nrow = 1) %*% t(pmf)))
+}
+
+var_score <- function(pmf){
+    if(NCOL(pmf)==1) pmf <- matrix(pmf, nrow = 1)
+    mu <- mean_score(pmf)
+    scores <- matrix(seq(0, ncol(pmf)-1), nrow = nrow(pmf), ncol = ncol(pmf), byrow = T)
+    unname(sweep(scores, 1, drop(mu))^2 %*% t(pmf))
+}
+
 #' Calculate mean and SD as a function of PSI
 #'
 #' @param model An irt_model object
@@ -48,6 +77,7 @@ calculate_sd_score_vs_psi <- function(model, psi_range = c(-4,4)){
     ) 
 }
 
+
 #' @export
 #' @rdname calculate_e_score_vs_psi
 plot_sd_score_vs_psi <- function(model, ...){
@@ -58,7 +88,43 @@ plot_sd_score_vs_psi <- function(model, ...){
         ggplot2::geom_line()
 }
 
-#' Determine IRT-based mean and SD link for CV model
+#' Calculate SD(zscore) as a function of PSI
+#'
+#' @param model An irt_model object
+#' @param psi_range A vector of lenght 2 specifying the lower and upper boundary for the latent variable
+#'
+#' @return A data.frame or a plot 
+#' @export
+calculate_sd_zscore_vs_psi <- function(model, psi_range = c(-4, 4)){
+    psi_grid <- seq(psi_range[1], psi_range[2], length.out = 100)
+    pmf <- pmf_ts(model, psi_grid)
+    scores <- matrix(seq(0, ncol(pmf)-1), nrow = 1)
+    pmf <- pmf[,-c(1, ncol(pmf))]
+    mx <- max(scores)
+    pscores <- scores/mx
+    pscores <- pscores[-c(1, length(pscores))]
+    zscores <- drop(qnorm(pscores))
+    mu <- pmf %*% zscores
+    
+    var <- diag(t(vapply(mu, function(mu_i)  zscores-mu_i, FUN.VALUE = zscores))^2 %*% t(pmf))
+    tibble::tibble(
+        psi = psi_grid,
+        sd_zscore = sqrt(var)
+    )
+}
+
+#' @export
+#' @rdname calculate_sd_zscore_vs_psi
+plot_sd_zscore_vs_psi <- function(model, ...){
+    df <- calculate_sd_zscore_vs_psi(model, ...)
+    ggplot2::ggplot(df, aes(psi, sd_zscore))+
+        ggplot2::xlab("PSI")+
+        ggplot2::ylab("sd(zscore)")+
+        ggplot2::geom_line()
+}
+
+
+#' Determine IRT-based links for CV or BI models
 #'
 #' @param model 
 #' @param psi_range 
@@ -78,6 +144,7 @@ calculate_cv_irt_link <- function(model,
                                   max_degree = 100){
     mirt_model <- as_mirt_model(model)
     result <- list()
+    result$type <- "score"
     # function calculatingirt_ts the mean score
     f_mean <- function(x) mirt::expected.test(mirt_model, matrix(x))
     # function calculating the sd score
@@ -166,9 +233,106 @@ calculate_cv_irt_link <- function(model,
 }
 
 
+#' @rdname calculate_cv_irt_link
+#' @export
+calculate_bi_irt_link <- function(model, 
+                                  psi_range = NULL, 
+                                  score_range = c(-Inf, Inf),
+                                  range_tol = 0.3,
+                                  approx_tol_sd  = 0.01, 
+                                  max_degree = 100){
+    mirt_model <- as_mirt_model(model)
+    result <- list()
+    result$type <- "zscore"
+    # function calculatingirt_ts the mean score
+    f_mean <- function(x) mirt::expected.test(mirt_model, matrix(x))
+    # function calculating the sd score
+    f_sd <- function(psi){
+        pmf <- pmf_ts(mirt_model, psi) 
+        scores <- matrix(seq(0, ncol(pmf)-1), nrow = 1)
+        pmf <- pmf[,-c(1, ncol(pmf))]
+        mx <- max(scores)
+        pscores <- scores/mx
+        pscores <- pscores[-c(1, length(pscores))]
+        zscores <- drop(qnorm(pscores))
+        mu <- pmf %*% zscores
+        
+        sqrt(diag(t(vapply(mu, function(mu_i)  zscores-mu_i, FUN.VALUE = zscores))^2 %*% t(pmf)))
+    }
+    if(is.null(psi_range)){
+        # determine psi range for approximation
+        max_range <- total_score_range(model$scale)
+        if(!is.finite(score_range[1])) score_range[1] <- max_range[1]
+        if(!is.finite(score_range[2])) score_range[2] <- max_range[2]
+        psi_interval <- c(-50, 50)
+        # solve score_min+range_tol = f_mean(psi) using root finder
+        sol_min <- uniroot(function(psi) f_mean(psi) - score_range[1] - range_tol, psi_interval)
+        # solve score_max-range_tol = f_mean(psi) using root finder
+        sol_max <- uniroot(function(psi) f_mean(psi) - score_range[2] + range_tol, psi_interval)
+        psi_range <- c(sol_min$root, sol_max$root)
+    }
+    result$range <- psi_range
+    psi_grid <- seq(psi_range[1], psi_range[2], length.out = 100)
+    # determine the polynomial degree necessary to approx mean fun with requested tol
+    # degree <- 0
+    # f_true <- f_mean(psi_grid)
+    # repeat{
+    #     degree <- degree+1
+    #     f_approx <- pracma::chebApprox(psi_grid, f_mean, psi_range[1], psi_range[2], degree)
+    #     error <- max(abs(f_true-f_approx))  
+    #     if(error < approx_tol_mean || degree>max_degree ) {
+    #         break
+    #     }
+    # }
+    # # determine Chebyshev polynomial coefficients
+    # coef_cheb <-  pracma::chebCoeff(f_mean, psi_range[1], psi_range[2], degree)
+    # poly_cheb <- pracma::chebPoly(degree)
+    # coef <- rev(drop(coef_cheb %*% poly_cheb))
+    # coef[1] <- coef[1] - coef_cheb[1]/2
+    # result$mean <- list(degree = degree, 
+    #                     psi = psi_grid, 
+    #                     true = f_true, 
+    #                     approx = f_approx,
+    #                     coefficients = coef)
+    # determine the polynomial degree necessary to approx sd fun with requested tol
+    degree <- 0
+    f_true <- f_sd(psi_grid)
+    repeat{
+        degree <- degree+1
+        f_approx <- pracma::chebApprox(psi_grid, f_sd, psi_range[1], psi_range[2], degree)
+        error <- max(abs(f_true-f_approx))  
+        if(error < approx_tol_sd || degree>max_degree ) {
+            break
+        }
+    }
+    # determine Chebyshev polynomial coefficients
+    coef_cheb <-  pracma::chebCoeff(f_sd, psi_range[1], psi_range[2], degree)
+    poly_cheb <- pracma::chebPoly(degree)
+    coef <- rev(drop(coef_cheb %*% poly_cheb))
+    coef[1] <- coef[1] - coef_cheb[1]/2
+    result$sd <- list(degree = degree, 
+                      psi = psi_grid, 
+                      true = f_true, 
+                      approx = f_approx, 
+                      coefficients = coef)
+    # determine approximation quality of the derivatives
+    # poly_mu <- rev(result$mean$coefficients)
+    poly_sigma <- rev(result$sd$coefficients)
+    # poly_dmu_dpsi <- pracma::polyder(poly_mu)
+    poly_dsigma_dpsi <- pracma::polyder(poly_sigma)
+    psi_t  <-  (2*psi_grid-(psi_range[2]+psi_range[1]))/(psi_range[2]-psi_range[1])
+    # result$mean$deriv_approx <- pracma::polyval(poly_dmu_dpsi, psi_t)*2/(psi_range[2]-psi_range[1])
+    # result$mean$deriv_true <- pracma::fderiv(f_mean, psi_grid)
+    result$sd$deriv_approx <- pracma::polyval(poly_dsigma_dpsi, psi_t)*2/(psi_range[2]-psi_range[1])
+    result$sd$deriv_true <- pracma::fderiv(f_sd, psi_grid)
+    return(result)
+}
+
+
+
 #' @export
 #' @rdname calculate_cv_irt_link
-plot_cv_irt_link <- function(res, plot_derivatives=FALSE){
+plot_irt_link <- function(res, plot_derivatives=FALSE){
     df <- res[c("mean","sd")] %>% 
         purrr::map(`[`, c("approx","true","psi")) %>% 
         purrr::map_dfr(tibble::as_tibble, 
@@ -189,10 +353,13 @@ plot_cv_irt_link <- function(res, plot_derivatives=FALSE){
             .id = "type"
         ) %>% 
             dplyr::mutate(
-                type = factor(.data$type, levels = c("function", "derivative"))
+                type = factor(.data$type, levels = c("function", "derivative")),
+                stat = factor(.data$stat, levels = c("mean", "sd"), labels = sprintf(c("mean(%s)", "sd(%s)"), res$type))
             )
     }
-    df <- df %>% dplyr::mutate(name = factor(.data$name, levels = c("true", "approx")))
+    df <- df %>% 
+        dplyr::mutate(name = factor(.data$name, levels = c("true", "approx")),
+                      )
     
     p <- ggplot2::ggplot(df, aes(psi, value, color = name, linetype = name)) +
         ggplot2::geom_line(size = 1) +
@@ -209,6 +376,18 @@ plot_cv_irt_link <- function(res, plot_derivatives=FALSE){
     p
 }
 
+nm_polynom <- function(poly_coef, digits = 6){
+    purrr::imap_chr(poly_coef, ~glue::glue("{round(.x, digits)}*TLV**{.y-1}")) %>% 
+        purrr::reduce(~paste(.x, .y, sep = ifelse(startsWith(.y,"-"),"","+"))) %>% 
+        sub(x = ., pattern = "*TLV**0", replacement = "", fixed = TRUE) %>% 
+        sub(x = ., pattern = "TLV**1", replacement = "TLV", fixed = TRUE)
+}
+
+nm_range_transform <- function(res, variable = "LV", digits = 6){
+    glue::glue("TLV = (2*{variable}-({b+a}))/{b-a}", 
+               a = round(res$range[1], digits), 
+               b = round(res$range[2], digits))
+}
 
 get_nm_cv_irt_link <- function(res, digits = 6){
     range_transform <- glue::glue("TLV = (2*LV-({b+a}))/{b-a}", 
@@ -221,6 +400,7 @@ get_nm_cv_irt_link <- function(res, digits = 6){
         sub(x = ., pattern = "TLV**1", replacement = "TLV", fixed = TRUE)
     mean_poly <- res$mean$coefficients %>% create_polynom
     sd_poly <- res$sd$coefficients %>% create_polynom
+    
     cg <- code_generator() %>% 
         add_line(range_transform) %>% 
         add_line("SCORE =", mean_poly) %>% 
@@ -252,5 +432,4 @@ calculate_cv_irt_information <- function(res, psi_range = c(-4, 4)){
         information = f_information(psi)
     )
 }
-
 
